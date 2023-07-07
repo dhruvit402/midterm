d6093abf0e80ee883c8dd75715b9b1118c93204f
dhruvit402 <126306791+dhruvit402@users.noreply.github.com>
2023-07-07 13:45:53 -0400

    first commit

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+url:"mongodb+srv://dvb402:dvb402@cluster0.rlwmnsa.mongodb.net/new?retryWrites=true&w=majority"
+
+};
